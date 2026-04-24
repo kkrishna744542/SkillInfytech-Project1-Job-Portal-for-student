8d7970715efaee0159c0f2c087661c4f2f09ef7f
Rohit Raj <rohitraj8875ind@gmail.com>
2026-04-24 16:05:30 +0530

    Update email.js

--- services/email.js
+++ services/email.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 async function createTransporter() {
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT || 587),
+    port: Number(process.env.SMTP_PORT || 465),
     secure: process.env.SMTP_SECURE === "true",
     auth: {
       user: process.env.SMTP_USER,
